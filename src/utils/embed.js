09665d8e03d5233e0489c95864fa64c358817be1
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-02-14 17:43:34 +0530

    Update embed.js

--- src/utils/embed.js
+++ src/utils/embed.js
@@ -6,6 +6,11 @@ import config from '../config';
 export const embed = props => ({
   embed: {
     color: config.color,
+    author: {
+		name: 'Three.js docs',
+		icon_url: 'Three.js docs","https://aws1.discourse-cdn.com/standard17/uploads/threejs/original/2X/c/c74c5243388bbfa21a39c3e824ddba702a623dec.png',
+		url: 'https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene',
+	},
     timestamp: new Date(),
     ...props,
   },

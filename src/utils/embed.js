c2f43ec4037cbf118bc9339819ab4885827ab64f
Cody Bennett <hi@codyb.co>
2021-02-14 06:38:41 -0600

    Fix embed link.

--- src/utils/embed.js
+++ src/utils/embed.js
@@ -10,7 +10,7 @@ export const embed = props => ({
       name: 'Three.js docs',
       icon_url:
         'https://aws1.discourse-cdn.com/standard17/uploads/threejs/original/2X/c/c74c5243388bbfa21a39c3e824ddba702a623dec.png',
-      url: 'https://threejs.org/docs',
+      url: 'https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene',
     },
     timestamp: new Date(),
     ...props,

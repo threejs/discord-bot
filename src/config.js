97ca3bf05c159e491b0adb5bf57bd7383a5aea71
Cody Bennett <hi@codyb.co>
2021-02-14 23:24:39 -0600

    Use env endpoint and icon in embeds.

--- src/config.js
+++ src/config.js
@@ -8,6 +8,9 @@ dotenv.config();
 const config = {
   apiEndpoint: process.env.APIENDPOINT || 'https://threejs.org/docs/index.html#',
   docsEndpoint: process.env.DOCSENDPOINT || 'https://threejs.org/docs/list.json',
+  icon:
+    process.env.ICON ||
+    'https://rawcdn.githack.com/mrdoob/three.js/dc2b340839e0fb1cf810a08dfbccf534121606b8/icon.png',
   color: process.env.COLOR || 0x049ef4,
   prefix: process.env.PREFIX || '!',
   token: process.env.TOKEN,

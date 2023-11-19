cbaa1f6aecc4abbf45d7eebaa3753f3d28f2ef68
Cody Bennett <hi@codyb.co>
2023-11-19 15:39:11 -0600

    refactor: serverless (#37)

---
+++ src/build.js
@@ -0,0 +1,112 @@
+import { config } from 'dotenv'
+import { fetch } from 'undici'
+import { JSDOM } from 'jsdom'
+import { compressSync } from 'lz4-napi'
+import fs from 'fs'
+import path from 'path'
+import { commands, getRevision } from './index.js'
+
+// Sync node env
+config()
+
+// Update command meta
+await fetch(
+  `https://discord.com/api/v8/applications/${process.env.APPLICATION_ID}/guilds/${process.env.GUILD}/commands`,
+  {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bot ${process.env.TOKEN}`,
+    },
+    body: JSON.stringify(commands),
+  },
+).then(async (res) => !res.ok && console.error(JSON.stringify(await res.json())))
+
+// Fetch revision
+const revision = await getRevision()
+
+// Fetch docs
+const DOCS_PATH = 'http://localhost:8080/docs' ?? 'https://threejs.org/docs'
+const DOCS_PROPS_REGEX = /^\[(property|method):[^\s]+\s([^\]]+)\].*/gi
+
+const flatten = (o) =>
+  Object.keys(o).reduce((a, v) => ({ ...a, ...(typeof o[v] === 'object' ? flatten(o[v]) : { [v]: o[v] }) }), {})
+
+const transform = ({ url, title, description, ...rest }) => ({
+  url,
+  title: title
+    // Remove labels from bracket syntax
+    .replace(/\[[^:]+:([^\s]+)\s([^\]]+)\s*\]/g, '$2: $1')
+    // Move method return type to end
+    .replace(/(\w+)(\:[^\(]+)(\([^\)]+\))/, '$1$3$2'),
+  description: description
+    ?.trim()
+    // Trim whitespace
+    .replace(/(\<br\>)+/g, ' ')
+    .replace(/\s+/g, ' ')
+    // Transform custom links
+    .replace(/\[link:([^\s]+)\s([^\]]+)\]/g, '<a href="$1">$2</a>')
+    // Cleanup inline links
+    .replace(/\[[^:]+:([^\s\]]+\s)?([^\]]+)\]/g, '$2'),
+  ...rest,
+})
+
+const docsEntries = await fetch(`${DOCS_PATH}/list.json`)
+  .then((res) => res.json())
+  .then((data) => flatten(data['en']))
+
+const properties = []
+const docs = await Promise.all(
+  Object.entries(docsEntries).map(async ([name, endpoint]) => {
+    const url = `${DOCS_PATH}/${endpoint}.html`
+
+    // Fetch page source, reformat self-references
+    const source = (await fetch(url).then((res) => res.text())).replace(/(:)this|\[name\]/g, `$1${name}`)
+    const { document } = new JSDOM(source).window
+    const elements = Array.from(document.body.children)
+
+    // Parse constructor meta
+    const constructor = elements.find((node) => node.outerHTML.includes('Constructor'))
+    const title = constructor?.nextElementSibling.textContent ?? name
+    const description = document.querySelector('.desc')?.textContent
+
+    for (const element of document.querySelectorAll('h3')) {
+      if (!DOCS_PROPS_REGEX.test(element.textContent)) continue
+
+      const title = element.textContent.replace(/\n.*/g, '')
+      const propertyName = title.replace(DOCS_PROPS_REGEX, '$2')
+      const description = element.nextElementSibling?.tagName === 'P' ? element.nextElementSibling.innerHTML : undefined
+      const url = `${DOCS_PATH}/#${endpoint}.${propertyName}`
+
+      properties.push(transform({ name: `${name}.${propertyName}`, title: `${name}.${title}`, description, url }))
+    }
+
+    return transform({ name, title, description, url })
+  }),
+)
+
+// Fetch examples
+const EXAMPLES_PATH = 'http://localhost:8080/examples' ?? 'https://threejs.org/examples'
+
+const examplesEntries = await fetch(`${EXAMPLES_PATH}/files.json`).then((res) => res.json())
+const examples = []
+
+for (const key in examplesEntries) {
+  const collection = examplesEntries[key]
+  for (const name of collection) {
+    examples.push({
+      name,
+      url: `${EXAMPLES_PATH}/#${name}`,
+      title: name.replace(/_/g, ' '),
+      thumbnail: {
+        url: `${EXAMPLES_PATH}/screenshots/${name}.jpg`,
+      },
+    })
+  }
+}
+
+// Write to disk
+fs.writeFileSync(
+  path.join(process.cwd(), 'api/data.lz4'),
+  compressSync(JSON.stringify({ revision, docs, properties, examples })),
+)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,249 @@
-import Bot from './bot';
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+import { uncompressSync } from 'lz4-napi'
+import { fetch } from 'undici'
+import { verifyKey, InteractionType, InteractionResponseType } from 'discord-interactions'
 
-const bot = new Bot();
-bot.start();
+// prettier-ignore
+try{var data=JSON.parse(uncompressSync(readFileSync(resolve(process.cwd(),'api/data.lz4'))))}catch(_){}
+
+/**
+ * Fuzzy searches three.js source for a query.
+ */
+function search(type, query) {
+  // Filter to properties on prop/method delimiter
+  if (type === 'docs' && /\.|#/.test(query)) type = 'properties'
+
+  // Search for exact result and recursive search for properties
+  const exactResult = data[type].find(({ name }) =>
+    name.includes('_')
+      ? name.toLowerCase() === query.replace(/\s/g, '_').toLowerCase()
+      : name.toLowerCase() === query.toLowerCase(),
+  )
+  if (exactResult) return [exactResult]
+
+  // Fuzzy search for related matches
+  const fuzzySearch = new RegExp(`.*${query.split('').join('.*')}.*`, 'i')
+  const results = data[type]
+    .filter((entry) => fuzzySearch.test(entry.name))
+    .sort((a, b) => a.name.localeCompare(b.name))
+
+  return results
+}
+
+export const commands = [
+  {
+    name: 'docs',
+    description: 'Searches https://threejs.org/docs for docs.',
+    options: [
+      {
+        name: 'query',
+        description: 'A query or class to search matching docs for',
+        type: 3,
+        max_length: 80,
+        required: true,
+        autocomplete: true,
+      },
+    ],
+    run({ query }) {
+      // Fuzzy search for matching docs
+      const results = search('docs', query)
+
+      switch (results.length) {
+        // Handle no matches
+        case 0:
+          return {
+            title: `Docs for "${query}"`,
+            description: `No documentation was found for \`${query}\`.`,
+          }
+        // Handle single match
+        case 1:
+          return results[0]
+        // Handle multiple matches
+        default:
+          return {
+            title: `Docs for "${query}"`,
+            description: `No documentation was found for \`${query}\`.\n\nRelated docs:`,
+            entries: results.map(({ name, url }) => `**[${name}](${url})**`),
+          }
+      }
+    },
+  },
+  {
+    name: 'examples',
+    description: 'Searches https://threejs.org/examples for examples.',
+    options: [
+      {
+        name: 'query',
+        description: 'A query or class to search matching docs for',
+        type: 3,
+        max_length: 80,
+        required: true,
+        autocomplete: true,
+      },
+    ],
+    run({ query }) {
+      // Fuzzy search for matching examples
+      const results = search('examples', query)
+
+      switch (results.length) {
+        // Handle no matches
+        case 0:
+          return {
+            title: `Examples for "${query}"`,
+            description: `No examples were found for \`${query}\`.`,
+          }
+        // Handle single match
+        case 1:
+          return results[0]
+        // Handle multiple matches
+        default:
+          return {
+            title: `Examples for "${query}"`,
+            description: `No example was found for \`${query}\`.\n\nRelated examples:`,
+            entries: results.map(({ title, url }) => `**[${title}](${url})**`),
+          }
+      }
+    },
+  },
+  {
+    name: 'help',
+    description: "Displays this bot's commands.",
+    run() {
+      const entries = commands.map(({ name, description }) => `**/${name}** - ${description}`)
+      return { title: 'Commands', entries }
+    },
+  },
+]
+
+/**
+ * Formats a command response into message data.
+ */
+function formatData(output, options = {}, page = 0) {
+  // Format message string
+  if (typeof output === 'string') return { content: output }
+
+  // Format message entries into navigable pages of 10
+  if (output.entries?.length) {
+    const pageLength = Math.ceil(output.entries.length / 10)
+    const pageIndex = page * 10
+    output.entries = output.entries.slice(pageIndex, pageIndex + 10)
+
+    Object.assign(output, {
+      description: output.entries.reduce((acc, entry) => `${acc}\n• ${entry}`, output.description ?? ''),
+      footer: { text: `Page ${page + 1} of ${pageLength}` },
+      components: pageLength > 1 && [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              style: 2,
+              custom_id: `0${new URLSearchParams({ ...options, page: 0 })}`,
+              disabled: page === 0,
+              label: '<<',
+            },
+            {
+              type: 2,
+              style: 2,
+              custom_id: `1${new URLSearchParams({ ...options, page: page - 1 })}`,
+              disabled: page === 0,
+              label: '← Back',
+            },
+            {
+              type: 2,
+              style: 2,
+              custom_id: `2${new URLSearchParams({ ...options, page: page + 1 })}`,
+              disabled: page === pageLength - 1,
+              label: 'Next →',
+            },
+            {
+              type: 2,
+              style: 2,
+              custom_id: `3${new URLSearchParams({ ...options, page: pageLength - 1 })}`,
+              disabled: page === pageLength - 1,
+              label: '>>',
+            },
+          ],
+        },
+      ],
+    })
+  }
+
+  const { content, flags, title, description, url, thumbnail, footer, components } = output
+
+  return {
+    content,
+    flags,
+    embeds: title ? [{ title, description, url, thumbnail, footer }] : undefined,
+    components: components?.length ? components : undefined,
+  }
+}
+
+/**
+ * Gets the latest three.js revision.
+ */
+export async function getRevision() {
+  const response = await fetch('https://raw.githubusercontent.com/mrdoob/three.js/master/src/constants.js')
+  if (!response.ok) throw new Error(response.statusText)
+
+  const constants = await response.text()
+  return constants.match(/(?!REVISION[^\d]+)(\d+)/)[0]
+}
+
+export default async (request, response) => {
+  // Validate request signature
+  const verified = verifyKey(
+    JSON.stringify(request.body),
+    request.headers['x-signature-ed25519'],
+    request.headers['x-signature-timestamp'],
+    process.env.PUBLIC_KEY,
+  )
+  if (!verified) return response.status(401).end()
+
+  // Update in background on new release
+  const revision = await getRevision()
+  if (revision !== data.revision) {
+    await fetch(process.env.VERCEL_DEPLOY_WEBHOOK, { method: 'POST' }).then(
+      async (res) => !res.ok && console.error(JSON.stringify(await res.json())),
+    )
+  }
+
+  // Handle interactions
+  switch (request.body.type) {
+    case InteractionType.PING:
+      return response.send({ type: InteractionResponseType.PONG })
+    case InteractionType.APPLICATION_COMMAND: {
+      const command = commands.find(({ name }) => name === request.body.data.name)
+      if (!command) return response.status(400).send({ error: 'Unknown command' })
+
+      const options = request.body.data.options?.reduce((acc, { name, value }) => ({ ...acc, [name]: value }), {})
+      const output = command.run(options)
+      const data = formatData(output, options)
+
+      return response.send({ type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE, data })
+    }
+    case InteractionType.MESSAGE_COMPONENT: {
+      const command = commands.find(({ name }) => name === request.body.message.interaction.name)
+      if (!command) return response.status(400).send({ error: 'Unknown command' })
+
+      const { page, ...options } = Object.fromEntries(new URLSearchParams(request.body.data.custom_id.substring(1)))
+      const output = command.run(options)
+      const data = formatData(output, options, Number(page))
+
+      return response.send({ type: InteractionResponseType.UPDATE_MESSAGE, data })
+    }
+    case InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE: {
+      const command = commands.find(({ name }) => name === request.body.data.name)
+      if (!command) return response.status(400).send({ error: 'Unknown command' })
+
+      const query = request.body.data.options.find(({ name }) => name === 'query')
+      const results = search(request.body.data.name, query.value)
+      const data = { choices: results.slice(0, 10).map(({ name }) => ({ name, value: name })) }
+
+      return response.send({ type: InteractionResponseType.APPLICATION_COMMAND_AUTOCOMPLETE_RESULT, data })
+    }
+    default:
+      return response.status(400).send({ error: 'Unknown interaction' })
+  }
+}
